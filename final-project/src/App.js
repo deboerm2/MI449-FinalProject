80182a832a121b9a9e9fee31843daaf6cb73bf70
deboerm2 <90107341+deboerm2@users.noreply.github.com>
2024-04-17 22:19:07 -0400

    progress

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -1,22 +1,88 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+
+const myHeaders = new Headers();
+myHeaders.append("Accept", "application/json");
+
+
+const requestOptions = {
+  method: "GET",
+  headers: myHeaders,
+  redirect: "follow"
+};
+/*
+const monsterList = fetch("https://www.dnd5eapi.co/api/monsters", requestOptions)
+  .then((response) => response.text())
+  .then((json) => json.results.map( monster => 
+  <p key = {monster.index}>
+    console.log(monster.name)
+  </p>
+  ))
+  //console.log(monsterList)
+  */
+
+  const response = await fetch(
+    "https://www.dnd5eapi.co/api/monsters", requestOptions
+  );
+  const monsterList = await response.json();
+  const response2 = await fetch(
+    "https://www.dnd5eapi.co/api/spells", requestOptions
+  );
+  const spellList = await response2.json();
+
+function ListMonsters(){
+   const output = monsterList.results.map( monster => 
+    
+    <li
+    key={monsterList.results.index}
+    style={{
+      color: 'red'
+    }}
+    >
+      {monster.name}
+    </li>
+  );
+  return(
+    output
+  )
+}
+function DisplayMonsterInfo(){
+  
+    
+}
+
+function ListSpells(){
+  const output = spellList.results.map( spell => 
+    <li
+    key={spell.index}
+    >
+    {spell.name}
+    </li> 
+    
+ 
+);
+ return(
+   output
+ )
+}
+async function CheckSpells(spell){
+  if( await fetch(`https://www.dnd5eapi.co/api/spells/${spell.index}`, requestOptions)
+  .then((response) => response.json)
+  .then((json) => json.hasOwnProperty('damage')))
+  {
+    return spell;
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React TESTING
-        </a>
+      <header className="App-Header">
+
+     <ListMonsters/>
+     <ListSpells/>
+        
       </header>
     </div>
   );
