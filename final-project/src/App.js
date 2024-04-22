2c77c434a781ab58c3377399ac876347bd06021d
deboerm2 <90107341+deboerm2@users.noreply.github.com>
2024-04-21 21:03:18 -0400

    css updates

--- final-project/src/App.js
+++ final-project/src/App.js
@@ -23,7 +23,7 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div className="App max-w-screen-lg">
      
      <div className='Results flex flex-row'>
      <DisplayMonsterInfo/> 
